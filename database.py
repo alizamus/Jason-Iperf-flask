a923554e5efdfa101553cbcfbdb52c03cfbf2326
Ali <alireza.zamani@cs.rutgers.edu>
2014-12-26 22:38:42 -0500

    here I add webpages for Row and column output

--- database.py
+++ database.py
@@ -9,15 +9,31 @@ c = conn.cursor()
 with open('test') as data_file:    
     data = json.load(data_file)
 
-c.execute("CREATE TABLE test (id integer, start_connected_socket integer, start_connected_local_host string, start_connected_local_port integer, start_connected_remote_host string, start_connected_remote_port integer)")
+c.execute("CREATE TABLE test (id integer, start_connected_socket integer, start_connected_local_host string, start_connected_local_port integer, start_connected_remote_host string, start_connected_remote_port integer, start_version string, start_system_info string, start_timestamp_time string, start_timestamp_timesecs integer, start_connecting_to_host string, start_connecting_to_port integer, start_cookie string, start_tcp_mss_default string, start_test_start_protocol string, start_test_start_num_streams integer, start_test_start_blksize integer, start_test_start_omit integer, start_test_start_duration integer, start_test_start_bytes integer, start_test_start_blocks integer, start_test_start_reverse integer)")
 #c.execute("INSERT INTO test VALUES (1, 2, 3, 4, 5)")
-c.execute('insert into test (id, start_connected_socket, start_connected_local_host, start_connected_local_port, start_connected_remote_host, start_connected_remote_port) values (?, ?, ?, ?, ?, ?)',
+c.execute('insert into test (id, start_connected_socket, start_connected_local_host, start_connected_local_port, start_connected_remote_host, start_connected_remote_port, start_version, start_system_info, start_timestamp_time, start_timestamp_timesecs, start_connecting_to_host, start_connecting_to_port, start_cookie, start_tcp_mss_default, start_test_start_protocol, start_test_start_num_streams, start_test_start_blksize, start_test_start_omit, start_test_start_duration, start_test_start_bytes, start_test_start_blocks, start_test_start_reverse) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
 			(1, 
 			data['start']['connected'][0]['socket'],
 			data['start']['connected'][0]['local_host'],
 			data['start']['connected'][0]['local_port'],
 			data['start']['connected'][0]['remote_host'],
-			data['start']['connected'][0]['remote_port']))
+			data['start']['connected'][0]['remote_port'],
+			data['start']['version'],
+			data['start']['system_info'],
+			data['start']['timestamp']['time'],
+			data['start']['timestamp']['timesecs'],
+			data['start']['connecting_to']['host'],
+			data['start']['connecting_to']['port'],
+			data['start']['cookie'],
+			data['start']['tcp_mss_default'],
+			data['start']['test_start']['protocol'],
+			data['start']['test_start']['num_streams'],
+			data['start']['test_start']['blksize'],
+			data['start']['test_start']['omit'],
+			data['start']['test_start']['duration'],
+			data['start']['test_start']['bytes'],
+			data['start']['test_start']['blocks'],
+			data['start']['test_start']['reverse']))
 
 conn.commit()
 myrows = c.execute("select * from test")
--- flask_database.py
+++ flask_database.py
@@ -51,8 +51,20 @@ def test_entries():
     myrows = g.db.execute("select * from test")
     for row in myrows:
 	print row
-    entries = [dict(title=str(row[0]), text1=str(row[1]), text2=str(row[2]), text3=str(row[3]), text4=str(row[4]), text5=str(row[5])) for row in cur.fetchall()]
+    entries = [dict(title=str(row[0]), text1=str(row[1]), text2=str(row[2]), text3=str(row[3]), text4=str(row[4]), text5=str(row[5]), text6=str(row[6]), text7=str(row[7]), text8=str(row[8]), text9=str(row[9]), text10=str(row[10]), text11=str(row[11]), text12=str(row[12]), text13=str(row[13]), text14=str(row[14]), text15=str(row[15]), text16=str(row[16]), text17=str(row[17]), text18=str(row[18]), text19=str(row[19]), text20=str(row[20]), text21=str(row[21])) for row in cur.fetchall()]
     return render_template('table.html', entries=entries)
+    #entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    #return (str(row))
+
+@app.route('/test2')
+def test_entries2():
+    #cur = g.db.execute('select id, name')
+    cur = g.db.execute("select * from test")
+    myrows = g.db.execute("select * from test")
+    for row in myrows:
+	print row
+    entries = [dict(title=str(row[0]), text1=str(row[1]), text2=str(row[2]), text3=str(row[3]), text4=str(row[4]), text5=str(row[5]), text6=str(row[6]), text7=str(row[7]), text8=str(row[8]), text9=str(row[9]), text10=str(row[10]), text11=str(row[11]), text12=str(row[12]), text13=str(row[13]), text14=str(row[14]), text15=str(row[15]), text16=str(row[16]), text17=str(row[17]), text18=str(row[18]), text19=str(row[19]), text20=str(row[20]), text21=str(row[21])) for row in cur.fetchall()]
+    return render_template('table_2.html', entries=entries)
     #entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     #return (str(row))
 
